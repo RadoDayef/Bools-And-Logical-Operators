64a698fe21106df84af5b16192581b40b90b9f0b
Maroooo <rado.dayef@gmail.com>
2026-02-25 07:58:57 +0200

    Full Video Code

--- bin/main.dart
+++ bin/main.dart
@@ -1,1 +1,16 @@
-void main() {}
+void main() {
+  bool a = true;
+  bool b = false;
+
+  // AND (&&)
+  print("a && b = ${a && b}");
+  print("a && true = ${a && true}");
+
+  // OR (||)
+  print("a || b = ${a || b}");
+  print("false || b = ${false || b}");
+
+  // NOT (!)
+  print("!a = ${!a}");
+  print("!b = ${!b}");
+}
